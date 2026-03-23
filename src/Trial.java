e1768ba85cdfbb3e0841160b9d9f80ee520f8b1d
gauravyss2412-cmd <gaurvyss2412@gmail.com>
2026-03-23 21:31:53 +0530

    started

--- src/Trial.java
+++ src/Trial.java
@@ -1,6 +1,7 @@
-
 public class Trial {
-    public static void main(String[] args){
-        System.out.println("tfk");
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
+
 }
